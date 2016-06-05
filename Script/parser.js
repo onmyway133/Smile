0134f551a3bac55c426098ecac3047fad468bffa
Khoa Pham <onmyway133@gmail.com>
2016-06-05 23:36:18 +0200

    Update scripts

--- Script/parser.js
+++ Script/parser.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var https = require('https');
+var fs = require('fs');
 
 function download(url, callback) {
 	https.get(url, function(res) {
@@ -18,10 +19,28 @@ function download(url, callback) {
 	});
 }
 
-download('https://github.com/github/gemoji/blob/master/db/emoji.json', function(data) {
+download('https://cdn.rawgit.com/github/gemoji/master/db/emoji.json', function(data) {
 	parse(data);
 });
 
 function parse(data) {
-	console.log(data)
+  const json = JSON.parse(data);
+
+  var string = 'let emojiList: [String: String] = [\n'
+  for (var i=0; i<json.length; ++i) {
+    const item = json[i];
+
+    if (typeof item.aliases === "undefined"
+    || typeof item.emoji === "undefined"
+    || item.emoji == "undefined") {
+      continue;
+    }
+
+    const itemString = '  "' + item.aliases[0] + '": "' + item.emoji + '",\n'
+    string = string + itemString
+  }
+
+  string = string + ']'
+
+  fs.writeFile('../Sources/Emoji.swift', string);
 };
--- Sources/Smile.swift
+++ Sources/Smile.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+
+let a: [String: String] = [
+  "": "",
+  "":"",
+]
